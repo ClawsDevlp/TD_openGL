3dc6b926646177ace73d0dd867add59b946ad531
Eva <eva.benharira@gmail.com>
2019-12-31 00:59:05 +0100

    initialisation scène 20 par 20 et modif angle et distance caméra

--- glimac/src/scene.cpp
+++ glimac/src/scene.cpp
@@ -3,7 +3,7 @@
 namespace glimac {
 
     //glm::vec3 Scenecoord = (0.);
-/* 
+    /* 
     void Scene::sceneInit(){
      	for (int y = 0; y < 1; ++y){
             for (int x = 0; x < 3; ++x){
@@ -14,17 +14,34 @@ namespace glimac {
         }
     } */
 
-    void Scene::sceneInit(GestionCube* ActualGerant){
-        for (int y = 0; y < 1; ++y){
-            for (int x = 0; x < 3; ++x){
-                for (int z = 0; z < 3; ++z){
-                    ActualGerant->ajoutCube(glm::vec3(x,y-1,z), glm::vec3(0,0,0));
-                }
-            }
+void Scene::sceneInit(GestionCube* ActualGerant){
+    for (int y = 0; y < 1; ++y){
+        for (int x = 0; x < 11; ++x){
+            for (int z = 0; z < 21; ++z){
+                if (z<=10){
+                    ActualGerant->ajoutCube(glm::vec3(-x,y-1,-z), glm::vec3(0,0,0));
+                } else {
+                    ActualGerant->ajoutCube(glm::vec3(-x,y-1,z-10), glm::vec3(0,0,0));
+                } 
+            } 
         }
     }
 
-/*     void Scene::dessinScene(GestionCube ActualGerant){
+    for (int y = 0; y < 1; ++y){
+        for (int x = 1; x < 11; ++x){
+            for (int z = 0; z < 21; ++z){
+                if (z<=10){
+                    ActualGerant->ajoutCube(glm::vec3(x,y-1,-z), glm::vec3(0,0,0));
+                } else {
+                    ActualGerant->ajoutCube(glm::vec3(x,y-1,z-10), glm::vec3(0,0,0));
+                } 
+            } 
+        }
+    }
+}
+
+
+    /*     void Scene::dessinScene(GestionCube ActualGerant){
         ActualGerant.dessinCube();
     } */
 }
