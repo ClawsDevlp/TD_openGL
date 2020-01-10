ef7229c1217c94abef2f39f1084edde2f8ec44ca
Claclicla <clara.daigmorte@gmail.com>
2020-01-10 15:42:07 +0100

    correction scene chargement + pdf

--- glimac/src/scene.cpp
+++ glimac/src/scene.cpp
@@ -76,7 +76,7 @@ void Scene::saveScene(GestionCube* ActualGerant){
 void Scene::savePositions(GestionCube* ActualGerant){
     //créer un fichier de sauvegarde sceneCube
     std::ofstream objetfichier;
-    objetfichier.open("../TD_openGL/sceneCube.txt", std::ios::out); //on ouvrre le fichier en ecriture
+    objetfichier.open("../world_Imaker/sceneCube.txt", std::ios::out); //on ouvrre le fichier en ecriture
     //permet de tester si le fichier s'est ouvert sans probleme 
     if (objetfichier.bad()){
         std::cout<<"échec de la création d'un fichier sauvegarde"<<std::endl;
@@ -91,7 +91,7 @@ void Scene::savePositions(GestionCube* ActualGerant){
 void Scene::saveColors(GestionCube* ActualGerant){
     //créer un fichier de sauvegarde sceneCube
     std::ofstream objetfichier;
-    objetfichier.open("../TD_openGL/sceneColors.txt", std::ios::out); //on ouvrre le fichier en ecriture
+    objetfichier.open("../world_Imaker/sceneColors.txt", std::ios::out); //on ouvrre le fichier en ecriture
     //permet de tester si le fichier s'est ouvert sans probleme 
     if (objetfichier.bad()){
         std::cout<<"échec de la création d'un fichier sauvegarde pour les couleurs"<<std::endl;
@@ -106,7 +106,7 @@ void Scene::saveColors(GestionCube* ActualGerant){
 void Scene::chargeScene(GestionCube* ActualGerant){
     //vider mongestionnaire de cube = optionnel
     //accéder à mon fichier de sauvegarde
-    std::ifstream objetfichier("../TD_openGL/sceneCube.txt");
+    std::ifstream objetfichier("../world_Imaker/sceneCube.txt");
     //déclaration d'un entier qui permettra de parcourir le fichier en question
     int lecture;
     std::vector<int> tabPos;
@@ -122,7 +122,7 @@ void Scene::chargeScene(GestionCube* ActualGerant){
     objetfichier.close();
 
     //De meme pour les données de couleurs
-    std::ifstream fichierCouleur("../TD_openGL/sceneColors.txt");
+    std::ifstream fichierCouleur("../world_Imaker/sceneColors.txt");
     float lectureColors;
     std::vector<float> tabCol;
     if (fichierCouleur.bad()){
@@ -143,33 +143,4 @@ void Scene::chargeScene(GestionCube* ActualGerant){
         ActualGerant->ajoutCube(glm::vec3(x,z,y),glm::vec3(tabCol[i],tabCol[i+1],tabCol[i+2]));
     }
 }
-/* 
-void Scene::chargeScene(GestionCube* ActualGerant){
-    //accéder à mon fichier de sauvegarde
-    std::ifstream objetfichier("../TD_openGL/sceneCube.txt");
-    //permet de tester si le fichier s'est ouvert sans probleme 
-    if (objetfichier.bad()){
-        std::cout<<"échec de l'ouverture du fichier sauvegarde"<<std::endl;
-    } 
-    //déclaration d'un entier qui permettra de parcourir le fichier en question
-    std::vector<glm::vec3> lecture;
-    //récupérer ses données et les rentrer dans mon gestionnaire de cube
-    //tant qu'on réussi à lire des données on les rentre dans le tableau qui gère nos cube
-    while(objetfichier>>lecture){
-        tab.push_back(lecture);
-    }
-    
-    for (int i = 0; i < sizeof(tab); i=i+3){
-        int x =tab[i];
-        int z =tab[i+1];
-        int y =tab[i+2];
-        //ActualGerant->cubesPositions.push_back(glm::vec3(x,z,y));
-        //ActualGerant->cubesCouleurs.push_back(glm::vec3(0,0,0));
-        ActualGerant->ajoutCube(glm::vec3(x,z,y),glm::vec3(0,0,0));
-    }
-}
- */
-    /*     void Scene::dessinScene(GestionCube ActualGerant){
-        ActualGerant.dessinCube();
-    } */
 }
